656227cd9a540ce5969defc39dbfb62ea123a0df
Damian Escobedo <damian050697@gmail.com>
2022-02-13 18:31:48 -0700

    add markdown styles

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,8 @@
     <article class="mt-6">
         <p class="text-2xl font-bold">{{ $post->title }}</p>
         <p class="mt-2 text-sm font-light opacity-50">{{ $post->created_at->format('F jS, Y') }}</p>
-        <div class="mt-4 leading-relaxed body">
-            <x-markdown>
+        <div class="mt-4 leading-relaxed body markdown">
+            <x-markdown theme="github-dark">
                 {!! $post->body !!}
             </x-markdown>
         </div>
